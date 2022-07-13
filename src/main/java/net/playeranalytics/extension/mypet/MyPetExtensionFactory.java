2635b35023f564d4d71cca942eb0f8bc7948c88b
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2022-07-13 10:24:55 +0300

    R1.2: Package change, junit scope

--- src/main/java/net/playeranalytics/extension/mypet/MyPetExtensionFactory.java
+++ src/main/java/net/playeranalytics/extension/mypet/MyPetExtensionFactory.java
@@ -20,33 +20,32 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
     THE SOFTWARE.
 */
-package com.djrapitops.extension;
+package net.playeranalytics.extension.mypet;
 
 import com.djrapitops.plan.extension.DataExtension;
-import com.djrapitops.plan.extension.extractor.ExtensionExtractor;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 /**
- * Test for the implementation of the new extension
+ * Factory for DataExtension.
  *
  * @author AuroraLS3
  */
-class ExtensionImplementationTest {
+public class MyPetExtensionFactory {
 
-    private ExtensionExtractor extractor;
-
-    @BeforeEach
-    void prepareExtractor() {
-        DataExtension extension = new MyPetExtension(true);
-        extractor = new ExtensionExtractor(extension);
+    private boolean isAvailable() {
+        try {
+            Class.forName("de.Keyle.MyPet.api.entity.MyPet");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 
-    @Test
-    @DisplayName("API is implemented correctly")
-    void noImplementationErrors() {
-        extractor.validateAnnotations();
+    public Optional<DataExtension> createExtension() {
+        if (isAvailable()) {
+            return Optional.of(new MyPetExtension());
+        }
+        return Optional.empty();
     }
-
 }
